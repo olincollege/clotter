12efe771b3274745cc4835f67b537d1eea2df16b
Aydin O'Leary <aydinoleary@gmail.com>
2023-02-21 11:53:52 -0500

    add build infrastructure and basic tests

---
+++ test/test_max.c
@@ -0,0 +1,21 @@
+#include <criterion/criterion.h>
+#include <criterion/new/assert.h>
+
+#include <stdlib.h>
+
+#include "../src/data_handling.h"
+
+Test(find_max, ascending_array) {
+  float numbers[5] = {1, 2, 3, 4, 5};
+  cr_assert(eq(flt, 5, find_max(numbers, 5)));
+}
+
+Test(find_max, negative_array) {
+  float numbers[5] = {-4, 0, -1, -2, -3};
+  cr_assert(eq(flt, 0, find_max(numbers, 5)));
+}
+
+Test(find_max, single_digit) {
+  float numbers[1] = {5.0};
+  cr_assert(eq(flt, 5.0, find_max(numbers, 1)));
+}
