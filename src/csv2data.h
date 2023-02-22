0ca23b2325cb83ff1175f8e2ac107b1a3a2f5b07
Sam Kaplan <skaplan1@olin.edu>
2023-02-22 17:09:29 -0500

    Add parsing for CSVs for bar graphs

---
+++ src/csv2data.h
@@ -0,0 +1,22 @@
+/**
+ * Utilites to parse CSVs for clotter implementation
+ */
+#pragma once
+
+#include <string.h>
+
+static const char WHITESPACE[] = " \f\n\r\t\v";
+
+/**
+ * Reads data in the following format:
+ * 
+ * SOURCE_1,val_1, val_2, val_3, val_4,...
+ * SOURCE_2,val_1, val_2, val_3, val_4,...
+ * SOURCE_3,val_1, val_2, val_3, val_4,...
+ * .
+ * .
+ * .
+ *
+ * @param filepath // assuming LINUS for now
+ */
+void csv2arr(char* file_path);
