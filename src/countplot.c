9a588bdeb150bfef79344960ff0149dc350d599a
Alex Butler <lxbtlr@pm.me>
2023-02-26 21:33:51 -0500

    refactored count plot and helper functions

--- src/countplot.c
+++ src/countplot.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
+
+#include"colors.c"
 #include<math.h>
+#include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 
 
-void blocks(float *scale){
+static void blocks(float* scale){
     // returns a string of length 20 filled with scale many blocks, resolution 1/8.
     char *whole_block = "\U00002588";
     int b_ = floor(*scale);
@@ -51,16 +53,49 @@ void blocks(float *scale){
     }
 }
 
+int count(char** names, float* values, float*b, int* num_colors){
+    // TODO: need to switch to using struct that we have defined, currently 
+    // using arrays
+    int data_size = sizeof(values) / sizeof(*values);
+    printf("Number of colors: %i, Number of bars: %i\n", *num_colors,data_size);
 
-//    int main(void){
-//        srand((int) time(NULL));
-//
-//        float r = 0;
-//        for (float j = 0; j<5; j++){
-//            printf("\n[ \t");
-//            r = (float)rand()/(float)(RAND_MAX/15);
-//            blocks(&r);
-//            printf("\n");
-//        }
-//        return 0;
-//    }
+    printf("\t\t\U0000250F\n");
+    for (int i = 0;i<data_size;i++){
+        // name of row
+        printf("%s\t", names[i]);
+
+        printf("\U0000252B");
+        switch(i%*num_colors){
+            case 6:
+                printf(MAG);
+                break; 
+            case 5:
+                printf(WHT);
+                break; 
+            case 4:
+                printf(BLU);
+                break; 
+            case 3:
+                printf(YEL);
+                break; 
+            case 2:
+                printf(CYN);
+                break; 
+            case 1:
+                printf(RED);
+                break;
+            case 0:
+                printf(GRN);
+                break;
+        }
+        blocks(&b[i]);
+        printf(RESET);
+        // value in row
+        printf("\t%f\n",values[i]);
+    }
+    printf("\t\t\U00002517\n");
+    return 0;
+}
+
+
+
