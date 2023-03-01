0ccce13ab3e051da17ffe0628a92d981b6316a28
Sam Kaplan <skaplan1@olin.edu>
2023-02-28 21:14:18 -0500

    Clean up argument handling and add support for plot type

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "csv2data.h"
 #include <stdio.h>
 #include <unistd.h>
 
@@ -7,42 +8,63 @@ https://www.tutorialspoint.com/getopt-function-in-c-to-parse-command-line-argume
 */
 
 int main(int argc, char *argv[]) {
-   int option;
-   // put ':' at the starting of the string so compiler can distinguish between '?' and ':'
-   while((option = getopt(argc, argv, ":ic:f:t:rx")) != -1){ //get option from the getopt() method
-      switch(option){
-         
-         // number of colors to be used
-         case 'c':
-            printf("number of colors: %s\n", optarg);
-            break;
+    // setting vars to write all arguments into
+    char* file_path;
+    char* plot_type;
+    int num_colors;
 
-        // type of plot desired by user
-        case 't':
-            printf("plot type:: %s\n", optarg);
-            break;
-         case 'i':
-         case 'l':
+    // need to set flags so we don't get a seg-fault if we try to read a value of 
+    // an optional arg that was not given
+    int fflag = 0;
+    int cflag = 0;
+    int tflag = 0;
 
-         
-         case 'r':
-            printf("Given Option: %c\n", option);
-            break;
+    extern char *optarg;
+	extern int optind;
+    int option;
+    // put ':' at the starting of the string so compiler can distinguish between '?' and ':'
+    while((option = getopt(argc, argv, "c:f:t:")) != -1){ //get option from the getopt() method
+        switch(option){
             
-        // path to csv file
-         case 'f': //here f is used for some file name
-            printf("csv file: %s\n", optarg);
-            break;
-         case ':':
-            printf("option needs a value\n");
-            break;
-         case '?': //used for some unknown options
-            printf("unknown option: %c\n", optopt);
-            printf("please use \"-f\" for file paths\n");
-            break;
-      }
-   }
-   for(; optind < argc; optind++){ //when some extra arguments are passed
-      printf("Given extra arguments: %s\n", argv[optind]);
-   }
+            // number of colors to be used
+            case 'c':
+                //num_colors = (int)(optarg);
+                break;
+
+            // type of plot desired by user
+            case 't':
+                tflag = 1;
+                plot_type = optarg;
+                break;
+    
+            // path to csv file
+            case 'f': 
+                fflag = 1;
+                file_path = optarg;
+                break;
+            case ':':
+                printf("option needs a value\n");
+                break;
+            case '?': //used for some unknown options
+                printf("unknown option: %c\n", optopt);
+                printf("please use \"-f\" for file paths\n");
+                break;
+        }
+    }
+    for(; optind < argc; optind++){ //when some extra arguments are passed
+        printf("Given extra arguments: %s\n", argv[optind]);
+    }
+
+    if(tflag){
+        printf("plot type: %s\n", plot_type);
+    }
+    
+    if(fflag){
+        printf("path to csv: %s\n", file_path);
+    }
+
+    
+
+
+
 }
