5dd313cb24fc31f8becf45359699506b1e554c0d
Aydin O'Leary <aydinoleary@gmail.com>
2023-03-09 18:21:28 -0500

    run clang-format on countplot.c

--- src/countplot.c
+++ src/countplot.c
@@ -1,13 +1,13 @@
-#include "data_types.h"
 #include "array_helpers.h"
 #include "constants.h"
+#include "data_types.h"
 
+#include <ctype.h>
 #include <math.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
-#include <ctype.h>
 
 // TODO: mode into a helper function
 
@@ -15,18 +15,17 @@ static Count df_to_count(Dataframe* df) {
 
   // place data values in its own array of length num_rows
   // for find_max_scalar and easier iteration later
-  float* values = (float*) malloc(sizeof(float) * df->num_cols);
+  float* values = (float*)malloc(sizeof(float) * df->num_cols);
   for (size_t i = 0; i < df->num_cols; i++) {
     values[i] = df->columns[i].numbers[0];
   }
-  
 
   // initialize a scalar to keep plot lengths at a max length
   float max_scalar = find_max_scalar(values, df->num_cols, PLOT_WIDTH);
 
   // create array of length df->num_rows
   // to store the number of 1/8 charwidth blocks to plot
-  int* numblocks = (int*) malloc(sizeof(int) * df->num_cols);
+  int* numblocks = (int*)malloc(sizeof(int) * df->num_cols);
   for (size_t i = 0; i < df->num_cols; i++) {
     // scale by max_scalar to keep within plot bounds
     // scale by 1/8 because we plot with 1/8 resolution
@@ -42,17 +41,17 @@ static Count df_to_count(Dataframe* df) {
   return out_count;
 }
 
-static void pad(int width, int length){
+static void pad(int width, int length) {
   // pads a string by the width minus the length
-  //printf("%i %i",width, length);
-  for (int i = 0; i <(size_t)width-length ; i++) {
+  // printf("%i %i",width, length);
+  for (int i = 0; i < (size_t)width - length; i++) {
     printf(" ");
   }
 }
 
 static void blocks(int nblocks) {
-  // returns a string of length PLOT_WIDTH filled with scale many blocks, resolution
-  // 1/8.
+  // returns a string of length PLOT_WIDTH filled with scale many blocks,
+  // resolution 1/8.
   float scale = (float)(nblocks) / 8;
   int b_ = (int)floor(scale);
   char output[PLOT_WIDTH - b_];
@@ -63,39 +62,39 @@ static void blocks(int nblocks) {
   char* complement = "";
   // determine complement for blocks
   int state = nblocks % 8;
- 
+
   switch (state) {
-    case 1:
-      complement = BLOCK_1;
-      break;
-    case 2:
-      complement = BLOCK_2;
-      break;
-    case 3:
-      complement = BLOCK_3;
-      break;
-    case 4:
-      complement = BLOCK_4;
-      break;
-    case 5:
-      complement = BLOCK_5;
-      break;
-    case 6:
-      complement = BLOCK_6;
-      break;
-    case 7:
-      complement = BLOCK_7;
-      break;
-    case 8:
-      complement = BLOCK_8;
-      break;
-    case 0:
-      // for some godforsaken reason need this case to catch float to int
-      // weirdness
-      complement = "";
+  case 1:
+    complement = BLOCK_1;
+    break;
+  case 2:
+    complement = BLOCK_2;
+    break;
+  case 3:
+    complement = BLOCK_3;
+    break;
+  case 4:
+    complement = BLOCK_4;
+    break;
+  case 5:
+    complement = BLOCK_5;
+    break;
+  case 6:
+    complement = BLOCK_6;
+    break;
+  case 7:
+    complement = BLOCK_7;
+    break;
+  case 8:
+    complement = BLOCK_8;
+    break;
+  case 0:
+    // for some godforsaken reason need this case to catch float to int
+    // weirdness
+    complement = "";
   }
   printf("%s", complement);
-  pad(PLOT_WIDTH,b_);
+  pad(PLOT_WIDTH, b_);
 }
 
 int display_count(Count ct, int num_colors) {
@@ -103,15 +102,15 @@ int display_count(Count ct, int num_colors) {
   // -- need length of series array
   printf("Number of colors: %i, Number of bars: %zu\n", num_colors,
          ct.dataframe->num_cols);
-  pad(NAME_SPACE,0);
-  printf(" %s\n",TOP_LEFT_CORNER);
+  pad(NAME_SPACE, 0);
+  printf(" %s\n", TOP_LEFT_CORNER);
   for (size_t i = 0; i < ct.dataframe->num_cols; i++) {
     // name of row
     printf(" %s", ct.dataframe->columns[i].name);
     // pad size of name with total number of chars
-    
-    pad(NAME_SPACE,strlen(ct.dataframe->columns[i].name));
-    
+
+    pad(NAME_SPACE, strlen(ct.dataframe->columns[i].name));
+
     printf(LEFT_TICK);
 
     switch (i % num_colors) {
@@ -144,8 +143,8 @@ int display_count(Count ct, int num_colors) {
     printf("  %f\n", ct.dataframe->columns[i].numbers[0]);
   }
   free(ct.numblocks);
-  pad(NAME_SPACE,0);
-  printf(" %s\n",BOTTOM_LEFT_CORNER);
+  pad(NAME_SPACE, 0);
+  printf(" %s\n", BOTTOM_LEFT_CORNER);
   return 0;
 }
 // TODO: explain this test case
@@ -174,7 +173,7 @@ int main(void) {
       .numbers = &o4,
   };
 
-  Series* cols = (Series*) malloc(sizeof(Series) * 4);
+  Series* cols = (Series*)malloc(sizeof(Series) * 4);
   cols[0] = val1;
   cols[1] = val2;
   cols[2] = val3;
